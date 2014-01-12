153a127fda9b0c6bf2868f08bd5e9e133dd92378
Spenser N Porter <Spenser.Porter@gmail.com>
2014-01-11 19:01:17 -0500

    Assignment 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,7 @@
 require 'erb'
-
+require 'time'
+time = Time.new
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+template = ERB.new "The value of x is: <%= x %>. The current year is <%= time.year%>. In <%= x %> years, it will be <%= time.year + x %>"
+
 puts template.result(binding)
